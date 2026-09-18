4da282e8f55e9ab6cff3f65becc07f08dfe0d8b7
Ayumi <amk24x@gmail.com>
2026-09-18 15:03:02 +0900

    Create models and migrations

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import DashboardView, ScheduleInputView, ScheduleEditView, TaskRunView, SettingsView
+from .views import DashboardView, ScheduleInputView, ScheduleEditView, ScheduleUpdateView, TaskRunView, SettingsView
 
 app_name = 'schedules'
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('schedule_input/', ScheduleInputView.as_view(), name='schedule_input'),
-    path('schedule_edit/', ScheduleEditView.as_view(), name='schedule_edit'),
+    path('edit/', ScheduleEditView.as_view(), name='schedule_edit'),
+    path('<int:pk>/edit/', ScheduleUpdateView.as_view(), name='schedule_update'),
     path('task_run/', TaskRunView.as_view(), name='task_run'),
     path('settings/', SettingsView.as_view(), name='settings'),
 ]
