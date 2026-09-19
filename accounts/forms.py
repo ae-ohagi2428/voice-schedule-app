0879c332d8e3d814fe510231946714ba1d420cab
Ayumi <amk24x@gmail.com>
2026-09-19 19:52:36 +0900

    Add user registration with styled form and messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,25 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import BaseUserCreationForm
 
-class RegistrationForm(UserCreationForm):
+User = get_user_model()
+
+class RegistrationForm(BaseUserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'ユーザー名'
+        self.fields['password1'].label = 'パスワード'
+        self.fields['password2'].label = 'パスワード（確認）'
+
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError('このメールアドレスは既に登録されています。')
+        return email
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=150,
+        verbose_name="ユーザー名",
+    )
     email = models.EmailField(
         unique=True,
         verbose_name="メールアドレス" 
